105860f479f9c644e785943761b501bd421efbf6
mohmead mahdy <mohmedmahdy714@gmail.com>
2025-01-18 09:20:18 +0200

    main

---
+++ android/app/src/main/kotlin/com/example/amanh_news_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.amanh_news_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AMANHNews extends StatelessWidget {
+  const AMANHNews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      // routerConfig: ,
+    );
+  }
+}
